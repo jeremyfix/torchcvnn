065003fe437f8951ea519deb08786dac521a5ae5
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2024-02-13 18:09:53 +0100

    bretigny dataset loader

--- src/torchcvnn/datasets/bretigny/dataset.py
+++ src/torchcvnn/datasets/bretigny/dataset.py
@@ -32,6 +32,14 @@ import numpy as np
 class Bretigny(Dataset):
     r"""
     Bretigny Dataset
+
+    Arguments:
+        root: the root directory containing the npz files for Bretigny
+        fold: train (70%), valid (15%), or test (15%)
+        transform : the transform applied the cropped image
+        balanced: whether or not to use balanced labels
+        patch_size: the dimensions of the patches to consider (rows, cols)
+        patch_stride: the shift between two consecutive patches, default:patch_size
     """
 
     """
@@ -42,13 +50,21 @@ class Bretigny(Dataset):
     def __init__(
         self,
         root: str,
+        fold: str,
         transform=None,
         balanced: bool = False,
         patch_size: tuple = (128, 128),
         patch_stride: tuple = None,
     ):
         self.root = pathlib.Path(root)
+        self.transform = transform
 
+        self.patch_size = patch_size
+        self.patch_stride = patch_stride
+        if patch_stride is None:
+            self.patch_stride = patch_size
+
+        # Preload the data
         sar_filename = self.root / "bretigny_seg.npz"
         if not sar_filename.exists():
             raise RuntimeError(f"Cannot find the file {sar_filename}")
@@ -61,6 +77,37 @@ class Bretigny(Dataset):
             label_filename = self.root / "bretigny_seg_4ROI.npz"
         self.labels = np.load(label_filename)["arr_0"]
 
+        if not fold in ["train", "valid", "test"]:
+            raise ValueError(
+                f"Unrecognized fold {fold}. Should be either train, valid or test"
+            )
+
+        # Crop the data with respect to the fold
+        if fold == "train":
+            col_start = 0
+            col_end = int(0.70 * self.HH.shape[1])
+        elif fold == "valid":
+            col_start = int(0.70 * self.HH.shape[1]) + 1
+            col_end = int(0.85 * self.HH.shape[1])
+        else:
+            col_start = int(0.85 * self.HH.shape[1]) + 1
+            col_end = self.HH.shape[1]
+
+        self.HH = self.HH[:, col_start:col_end]
+        self.HV = self.HV[:, col_start:col_end]
+        self.VV = self.VV[:, col_start:col_end]
+        self.labels = self.labels[:, col_start:col_end]
+
+        # Precompute the dimension of the grid of patches
+        nrows = self.HH.shape[0]
+        ncols = self.HH.shape[1]
+
+        nrows_patch, ncols_patch = self.patch_size
+        row_stride, col_stride = self.patch_stride
+
+        self.nsamples_per_rows = (nrows - nrows_patch) // row_stride + 1
+        self.nsamples_per_cols = (ncols - ncols_patch) // col_stride + 1
+
     def __len__(self) -> int:
         """
         Returns the total number of patches in the whole image.
@@ -68,7 +115,7 @@ class Bretigny(Dataset):
         Returns:
             the total number of patches in the dataset
         """
-        return 0
+        return self.nsamples_per_rows * self.nsamples_per_cols
 
     def __getitem__(self, idx) -> Tuple[Any, Any]:
         """
@@ -78,6 +125,24 @@ class Bretigny(Dataset):
             idx (int): Index
 
         Returns:
-            tuple: (patch, labels) where patch contains the 4 complex valued polarization HH, HV, VH, VV and labels contains the aligned semantic labels
+            tuple: (patch, labels) where patch contains the 3 complex valued polarization HH, HV, VV and labels contains the aligned semantic labels
         """
-        pass
+        row_stride, col_stride = self.patch_stride
+        start_row = (idx // self.nsamples_per_cols) * row_stride
+        start_col = (idx % self.nsamples_per_cols) * col_stride
+        num_rows, num_cols = self.patch_size
+        patches = [
+            patch[
+                start_row : (start_row + num_rows), start_col : (start_col + num_cols)
+            ]
+            for patch in [self.HH, self.HV, self.VV]
+        ]
+        patches = np.stack(patches)
+        if self.transform is not None:
+            patches = self.transform(patches)
+
+        labels = self.labels[
+            start_row : (start_row + num_rows), start_col : (start_col + num_cols)
+        ]
+
+        return patches, labels

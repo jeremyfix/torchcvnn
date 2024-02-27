f301bce3e5a4b5f575d6186ee167b08a01005a76
Quentin GABOT <quentingabot@gmail.com>
2024-02-27 16:30:39 +0100

    mseloss

--- src/torchcvnn/nn/modules/loss.py
+++ src/torchcvnn/nn/modules/loss.py
@@ -23,59 +23,47 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# External imports
 import torch
 import torch.nn as nn
 
 
-class ComplexMeanSquareError(nn.Module):
-    def __init__(self):
-        super(ComplexMeanSquareError, self).__init__()
+class ComplexMSELoss(nn.modules.loss._Loss):
+    r"""A custom implementation of Mean Squared Error (MSE) Loss for complex-valued inputs.
 
-    def forward(self, y_true, y_pred):
-        # Ensure y_pred and y_true are torch tensors
-        y_pred = torch.as_tensor(y_pred)
-        y_true = torch.as_tensor(y_true)
+    This loss function is designed to compute the mean squared error between two sets of complex-valued tensors. Unlike the standard MSE loss which directly computes the square of the difference between predicted values and true values for real numbers, this implementation adapts to complex numbers by calculating the squared magnitude of the difference between the complex predicted and true values before averaging.
 
-        # Handle complex pred but real true
-        if torch.is_complex(y_pred) and not torch.is_complex(y_true):
-            y_true = torch.complex(y_true, torch.zeros_like(y_true))
+    For complex-valued numbers, the MSE is defined as:
+        MSE = mean(|y_true - y_pred|^2)
+    where |y_true - y_pred| denotes the magnitude of the complex difference between the true and predicted values. This formula ensures that both the real and imaginary parts of the complex numbers are considered in the loss calculation.
 
-        # Ensure y_true is the same dtype as y_pred
-        y_true = y_true.type_as(y_pred)
+    For real-valued numbers, the standard MSE formula is:
+        MSE = mean((y_true - y_pred)^2)
+    where (y_true - y_pred)^2 is the square of the difference between the true and predicted values.
 
-        # Calculate Mean Square Error
-        mse = torch.mean(torch.square(torch.abs(y_true - y_pred)))
+    Parameters:
+        size_average (bool, optional): Deprecated (use reduction). By default, the losses are averaged over each loss element in the batch. Note: when (reduce is False), size_average is ignored. Default is None.
+        reduce (bool, optional): Deprecated (use reduction). By default, the losses are averaged or summed over observations for each minibatch depending on reduction. Note: when (reduce is False), size_average is ignored. Default is None.
+        reduction (str, optional): Specifies the reduction to apply to the output: 'none' | 'mean' | 'sum'. 'none': no reduction will be applied, 'mean': the sum of the output will be divided by the number of elements in the output, 'sum': the output will be summed. Default is 'mean'.
 
-        return mse
+    Inputs:
+        y_pred (torch.Tensor): The predicted values, can be a complex tensor.
+        y_true (torch.Tensor): The ground truth values, must have the same shape as the predicted values, can be a complex tensor.
 
+    Returns:
+        torch.Tensor: The calculated mean squared error loss.
 
-class ComplexVAELoss(nn.Module):
-    def __init__(self):
-        """
-        Initializes the VAE Loss module.
+    Examples:
+        >>> loss = ComplexMSELoss()
+        >>> y_pred = torch.tensor([1+1j, 2+2j], dtype=torch.complex64)
+        >>> y_true = torch.tensor([1+2j, 2+3j], dtype=torch.complex64)
+        >>> output = loss(y_pred, y_true)
+        >>> print(output)
+    """
 
-        """
-        super(ComplexVAELoss, self).__init__()
+    def __init__(self, size_average=None, reduce=None, reduction: str = "mean") -> None:
+        super().__init__(size_average, reduce, reduction)
 
-    def forward(self, x, recon_x, mu, logvar):
-        """
-        Computes the VAE loss.
-
-        Parameters:
-        recon_x: Reconstructed data.
-        x: Original input data.
-        mu: Mean from the latent space.
-        logvar: Log variance from the latent space.
-
-        Returns:
-        torch.Tensor: Computed VAE loss.
-        """
-        # Reconstruction Loss
-        MSELoss = ComplexMeanSquareError()
-        recon_loss = MSELoss(y_true=x, y_pred=recon_x)
-
-        # KL Divergence
-        kl_divergence = torch.abs(
-            -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        )  # valeur absolue car la loss doit être réelle ?
-        return recon_loss + kl_divergence
+    def forward(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+        # Calculate Mean Square Loss for complex numbers
+        return torch.mean(torch.square(torch.abs(y_true - y_pred)))
--- tests/test_loss.py
+++ tests/test_loss.py
@@ -31,19 +31,9 @@ import torchcvnn.nn as c_nn
 def test_complex_mse_loss():
     y_true = torch.empty(1, 3, dtype=torch.complex64)
     y_pred = torch.empty(1, 3, dtype=torch.complex64)
-    loss_mse = c_nn.ComplexMeanSquareError()
+    loss_mse = c_nn.loss.ComplexMSELoss()
     loss_mse(y_true, y_pred)
-
-
-def test_complex_vae_loss():
-    y_true = torch.empty(1, 3, dtype=torch.complex64)
-    y_pred = torch.empty(1, 3, dtype=torch.complex64)
-    mu = torch.empty(1, 2, dtype=torch.complex64)
-    logvar = torch.empty(1, 2, dtype=torch.complex64)
-    loss_vae = c_nn.ComplexVAELoss()
-    loss_vae(y_true, y_pred, mu, logvar)
 
 
 if __name__ == "__main__":
     test_complex_mse_loss()
-    test_complex_vae_loss()

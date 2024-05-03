02dec30a27a89e42a266332cd247c0bc63c731e6
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2024-05-03 17:42:27 +0200

    getting a patch from a SLC file

--- src/torchcvnn/datasets/slc/slc_file.py
+++ src/torchcvnn/datasets/slc/slc_file.py
@@ -21,7 +21,11 @@
 # SOFTWARE.
 
 # Standard imports
+import os
 import pathlib
+
+# External imports
+import numpy as np
 
 # Local imports
 from .ann_file import AnnFile
@@ -82,9 +86,13 @@ class SLCFile:
     There is one SLC file per segment and per polarization.
     """
 
-    def __init__(self, filename):
+    def __init__(self, filename: str, patch_size: tuple, patch_stride: tuple = None):
         self.filename = pathlib.Path(filename)
         self.parameters = parse_slc_filename(self.filename.name)
+        self.patch_size = patch_size
+        self.patch_stride = patch_stride
+        if self.patch_stride is None:
+            self.patch_stride = patch_size
 
         # The annotation filename is almost the same as the SLC filename, except we drop
         # the segment number and downsample factor
@@ -94,17 +102,57 @@ class SLCFile:
 
         downsample_factor = self.parameters["downsample_factor"]
         segment_number = self.parameters["segment_number"]
-        self.azimuth_pixel_spacing = getattr(
-            self.ann_file, f"{downsample_factor}_slc_azimuth_pixel_spacing"
-        )
-        self.range_pixel_spacing = getattr(
-            self.ann_file, f"{downsample_factor}_slc_range_pixel_spacing"
-        )
-        self.global_average_squint_angle = self.ann_file.global_average_squint_angle
-        self.center_wavelength = self.ann_file.center_wavelength
+        # self.azimuth_pixel_spacing = getattr(
+        #     self.ann_file, f"{downsample_factor}_slc_azimuth_pixel_spacing"
+        # )
+        # self.range_pixel_spacing = getattr(
+        #     self.ann_file, f"{downsample_factor}_slc_range_pixel_spacing"
+        # )
+        # self.global_average_squint_angle = self.ann_file.global_average_squint_angle
+        # self.center_wavelength = self.ann_file.center_wavelength
         self.n_rows = getattr(
             self.ann_file, f"slc_{segment_number}_{downsample_factor}_rows"
         )
         self.n_cols = getattr(
             self.ann_file, f"slc_{segment_number}_{downsample_factor}_columns"
         )
+
+        # Precompute the dimension of the grid of patches
+        nrows_patch, ncols_patch = self.patch_size
+        row_stride, col_stride = self.patch_stride
+
+        self.nsamples_per_rows = (self.n_rows - nrows_patch) // row_stride + 1
+        self.nsamples_per_cols = (self.n_cols - ncols_patch) // col_stride + 1
+
+    def __len__(self):
+        """
+        Returns the number of patches that can be extracted from the SLC file
+        """
+        return self.nsamples_per_rows * self.nsamples_per_cols
+
+    def __getitem__(self, item):
+        """
+        Returns the item-th patch from the SLC file
+        """
+        assert 0 <= item < len(self)
+        # Compute the row and column index of the patch
+        row = item // self.nsamples_per_cols
+        col = item % self.nsamples_per_cols
+
+        # Compute the starting row and column index of the patch
+        row_stride, col_stride = self.patch_stride
+        row_start = row * row_stride
+        col_start = col * col_stride
+
+        # Read the patch
+        # The SLC file is a binary file of complex64
+        patch = np.zeros(self.patch_size, dtype=np.complex64)
+        with open(self.filename, "rb") as fh:
+            for row in range(self.patch_size[0]):
+                fh.seek(
+                    (row_start + row) * self.n_cols * 8 + col_start * 8, os.SEEK_SET
+                )
+                patch[row, :] = np.fromfile(
+                    fh, dtype=np.complex64, count=self.patch_size[1]
+                )
+        return patch

8ea82025fde55791887eade0c44f60df3b96af2d
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2024-12-11 17:45:01 +0100

    adds the phase extraction in addition to the magnitude

--- src/torchcvnn/datasets/mstar/dataset.py
+++ src/torchcvnn/datasets/mstar/dataset.py
@@ -24,10 +24,13 @@
 from typing import Tuple
 from glob import glob
 import pathlib
+import logging
+import struct
 
 # External imports
 import torch
 from torch.utils.data import Dataset
+import numpy as np
 
 
 def parse_header(fh):
@@ -45,7 +48,6 @@ def parse_header(fh):
         value = items[1].strip()
         parsed_fields[key] = value
         next_line = fh.readline().strip()
-    print(next_line + "ENNND")
 
     return parsed_fields
 
@@ -61,8 +63,40 @@ class MSTARSample:
 
             num_rows = int(self.header["NumberOfRows"])
             num_cols = int(self.header["NumberOfColumns"])
+            phoenix_header_length = int(self.header["PhoenixHeaderLength"])
+            native_header_length = int(self.header["native_header_length"])
 
-            # Read the data from the file
+            fh.seek(phoenix_header_length + native_header_length)
+
+            sig_size = int(self.header["PhoenixSigSize"])
+            bytes_per_values = (
+                sig_size - phoenix_header_length - native_header_length
+            ) // (2 * num_rows * num_cols)
+
+            if bytes_per_values == 4:
+                # Read the data as float32
+                pass
+            elif bytes_per_values == 2:
+                # Read the data as uint16
+                pass
+            else:
+                raise ValueError(
+                    f"Unsupported number of bytes per value : {bytes_per_values}"
+                )
+
+        # Read the data from the file
+        with open(self.filename, "rb") as fh:
+            fh.seek(phoenix_header_length + native_header_length)
+
+            data_bytes = fh.read(num_rows * num_cols * bytes_per_values)
+            unpacked = struct.unpack(">" + ("f" * num_cols * num_rows), data_bytes)
+            magnitudes = np.array(unpacked).reshape(num_rows, num_cols)
+
+            data_bytes = fh.read(num_rows * num_cols * bytes_per_values)
+            unpacked = struct.unpack(">" + ("f" * num_cols * num_rows), data_bytes)
+            phases = np.array(unpacked).reshape(num_rows, num_cols)
+
+            self.data = magnitudes * np.exp(1j * phases)
 
 
 class MSTARTargets(Dataset):
@@ -71,11 +105,12 @@ class MSTARTargets(Dataset):
         self.rootdir = pathlib.Path(rootdir)
         self.transform = transform
 
+        self.class_names = ["BMP2", "BTR70", "T82", "SLICY"]
         self.data_files = {}
         self.num_data_files = {}
         self.tot_num_data_files = 0
         if train:
-            for key in ["BMP2", "BTR70", "T82"]:
+            for key in self.class_names:
                 pattern = str(
                     self.rootdir / "TARGETS" / "TRAIN" / "17_DEG" / key / "**" / "HB*"
                 )
@@ -96,6 +131,13 @@ class MSTARTargets(Dataset):
             index -= self.num_data_files[key]
 
         filename = self.data_files[key][index]
+        logging.debug(f"Loading the MSTAR file {filename}")
+
         sample = MSTARSample(filename)
-        print(f"Loading {filename}...")
-        return None
+        class_idx = self.class_names.index(key)
+
+        data = sample.data
+        if self.transform is not None:
+            data = self.transform(data)
+
+        return data, class_idx

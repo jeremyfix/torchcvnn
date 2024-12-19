4c6f032f7e07d5e1c5fc23aa88d0f4ec5e03d250
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2024-12-19 12:00:47 +0100

    initial import of the code to examplify patching a RVNN into a CVNN

---
+++ examples/patched_TIMM.py
@@ -0,0 +1,206 @@
+# coding: utf-8
+
+# MIT License
+
+# Copyright (c) 2024 Jérémy Fix
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+# Example using complex valued neural networks to classify the SAMPLE data
+
+In this example, we will use the complex valued neural networks to classify the SAMPLE data. This sample script also shows how to patch a pre-constructed neural network, as provided by TIMM to make it complex valued.
+
+We benefit from timm to build the architecture but then replace the real valued modules by complex valued counterparts.
+
+Requires dependencies :
+    python3 -m pip install torchcvnn timm torchvision
+"""
+
+# Standard imports
+import random
+import argparse
+import logging
+
+# External imports
+import torch
+import torch.nn as nn
+from torchvision.transforms import v2
+import timm
+
+# Local imports
+import torchcvnn
+import torchcvnn.nn as c_nn
+import torchcvnn.datasets
+import utils
+
+
+def get_dataloaders(datadir, batch_size=64, valid_ratio=0.1):
+    transform = v2.Compose(
+        transforms=[v2.ToImage(), v2.Resize(128), v2.CenterCrop(128)]
+    )
+
+    train_valid_dataset = torchcvnn.datasets.SAMPLE(
+        datadir, transform=transform, download=True
+    )
+
+    all_indices = list(range(len(train_valid_dataset)))
+    random.shuffle(all_indices)
+    split_idx = int(valid_ratio * len(train_valid_dataset))
+    valid_indices, train_indices = all_indices[:split_idx], all_indices[split_idx:]
+
+    # Train dataloader
+    train_dataset = torch.utils.data.Subset(train_valid_dataset, train_indices)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True
+    )
+
+    # Valid dataloader
+    valid_dataset = torch.utils.data.Subset(train_valid_dataset, valid_indices)
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=batch_size, shuffle=False
+    )
+
+    num_classes = len(train_valid_dataset.class_names)
+
+    return train_loader, valid_loader, num_classes
+
+
+def convert_to_complex(module: nn.Module) -> nn.Module:
+    cdtype = torch.complex64
+    for name, child in module.named_children():
+        if isinstance(child, nn.Conv2d):
+            setattr(
+                module,
+                name,
+                nn.Conv2d(
+                    child.in_channels,
+                    child.out_channels,
+                    child.kernel_size,
+                    stride=child.stride,
+                    padding=child.padding,
+                    bias=child.bias is not None,
+                    dtype=cdtype,
+                ),
+            )
+
+        elif isinstance(child, nn.ReLU):
+            setattr(module, name, c_nn.CReLU())
+
+        elif isinstance(child, nn.BatchNorm2d):
+            setattr(module, name, c_nn.BatchNorm2d(child.num_features))
+
+        elif isinstance(child, nn.MaxPool2d):
+            setattr(
+                module,
+                name,
+                c_nn.MaxPool2d(
+                    child.kernel_size,
+                    stride=child.stride,
+                    padding=child.padding,
+                ),
+            )
+        elif isinstance(child, nn.Linear):
+            setattr(
+                module,
+                name,
+                nn.Linear(
+                    child.in_features,
+                    child.out_features,
+                    bias=child.bias is not None,
+                    dtype=cdtype,
+                ),
+            )
+        else:
+            convert_to_complex(child)
+
+    return module
+
+
+def train(datadir):
+    """
+    Train function
+
+    """
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    valid_ratio = 0.1
+    batch_size = 100
+    epochs = 10
+
+    # Dataloading
+    train_loader, valid_loader, num_classes = get_dataloaders(
+        datadir, batch_size=batch_size, valid_ratio=valid_ratio
+    )
+
+    X, _, _ = next(iter(train_loader))
+    in_chans = X.shape[1]
+
+    # Build the model as a patched TIMM
+    # and send it to the right device
+    real_valued_model = timm.create_model(
+        "resnet18", pretrained=False, num_classes=num_classes, in_chans=in_chans
+    )
+    model = convert_to_complex(real_valued_model)
+    # Add a final layer to the model to transform the complex valued logits into
+    # real valued logits to go into the CrossEntropyLoss
+    model = nn.Sequential(
+        model,
+        c_nn.Mod(),
+    )
+    model.to(device)
+
+    # Loss, optimizer, callbacks
+    f_loss = nn.CrossEntropyLoss()
+    optim = torch.optim.Adam(model.parameters(), lr=3e-4)
+    logpath = utils.generate_unique_logpath("./logs", "SAMPLE")
+    logging.info(f"Logging to {logpath}")
+    checkpoint = utils.ModelCheckpoint(model, logpath, 4, min_is_best=True)
+
+    # Training loop
+    for e in range(epochs):
+        print(">> Training")
+        train_loss, train_acc = utils.train_epoch(
+            model, train_loader, f_loss, optim, device
+        )
+
+        print(">> Testing")
+        valid_loss, valid_acc = utils.test_epoch(model, valid_loader, f_loss, device)
+        updated = checkpoint.update(valid_loss)
+        better_str = "[>> BETTER <<]" if updated else ""
+
+        print(
+            f"[Step {e}] Train : CE {train_loss:5.2f} Acc {train_acc:5.2f} | Valid : CE {valid_loss:5.2f} Acc {valid_acc:5.2f}"
+            + better_str
+        )
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser(description="SAMPLE classification with torchcvnn")
+    parser.add_argument(
+        "--datadir", type=str, default="data", help="Path to the data directory"
+    )
+
+    args = parser.parse_args()
+
+    datadir = args.datadir
+
+    train(datadir)
